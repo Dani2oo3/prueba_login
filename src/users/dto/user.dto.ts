30b8e1b77d3a7d644762e0b4c7419bf026d1e5a3
Daniel <danielfernandezmateos03@gmail.com>
2024-05-23 12:35:15 +0200

    swagger

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,31 +1,37 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Prop } from '@nestjs/mongoose';
-import { IsEmail, IsNotEmpty, IsString, MinLength, } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class UserDto {
+  @ApiProperty({ description: 'El nombre del usuario' })
   @IsString()
   readonly name: string;
 
+  @ApiProperty({ description: 'El correo electrónico del usuario', uniqueItems: true })
   @IsEmail()
   @IsNotEmpty()
-  @Prop({unique: true })
+  @Prop({ unique: true })
   readonly email: string;
 
+  @ApiProperty({ description: 'La contraseña del usuario' })
   @MinLength(6)
   @IsNotEmpty()
   readonly password: string;
 
+  @ApiProperty({ description: 'El número de teléfono del usuario' })
   @IsString()
   readonly phone: string;
 }
 
 export class ChangePasswordDto {
+  @ApiProperty({ description: 'El ID del usuario' })
   @IsNotEmpty()
   @IsString()
-  userId: string; // Identificador del usuario
+  userId: string;
 
+  @ApiProperty({ description: 'La nueva contraseña del usuario' })
   @IsNotEmpty()
   @IsString()
   @MinLength(6)
   newPassword: string;
 }
-
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,49 +1,65 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Delete,
-  Req,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ChangePasswordDto, UserDto } from './dto/user.dto';
 import { User } from './entities/user.entity';
+import { MessagePattern } from '@nestjs/microservices';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @MessagePattern({ cmd: 'register' })
   @Post('register')
+  @ApiOperation({ summary: 'Registrar un nuevo usuario' })
+  @ApiResponse({ status: 201, description: 'El usuario ha sido creado.' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   create(@Body() createUserDto: UserDto) {
     return this.usersService.create(createUserDto);
   }
 
+  @MessagePattern({ cmd: 'login' })
   @Post('login')
+  @ApiOperation({ summary: 'Iniciar sesión' })
+  @ApiResponse({ status: 200, description: 'Login exitoso.' })
+  @ApiResponse({ status: 401, description: 'Credenciales incorrectas.' })
   login(@Body() createUserDto: UserDto) {
     const { email, password } = createUserDto;
     return this.usersService.loginUser(email, password);
   }
-  
+
+  @MessagePattern({ cmd: 'getUsers' })
   @Get('getUsers')
+  @ApiOperation({ summary: 'Obtener todos los usuarios' })
+  @ApiResponse({ status: 200, description: 'Lista de usuarios.', type: [User] })
   async getAllUsers() {
     return await this.usersService.getAllUsers();
   }
 
+  @MessagePattern({ cmd: 'change-password' })
   @Post('change-password')
+  @ApiOperation({ summary: 'Cambiar contraseña' })
+  @ApiResponse({ status: 200, description: 'Contraseña cambiada exitosamente.' })
+  @ApiResponse({ status: 404, description: 'Usuario no encontrado.' })
   async changePassword(@Body() changePasswordDto: ChangePasswordDto) {
     return await this.usersService.changePassword(changePasswordDto);
   }
 
-  //eliminar por id
+  @MessagePattern({ cmd: 'delete:id' })
   @Delete(':id')
+  @ApiOperation({ summary: 'Eliminar usuario por ID' })
+  @ApiResponse({ status: 200, description: 'Usuario eliminado exitosamente.' })
+  @ApiResponse({ status: 404, description: 'Usuario no encontrado.' })
   async deleteUser(@Param('id') userId: string): Promise<void> {
     await this.usersService.deleteUser(userId);
   }
 
-  //encontrar por email
+  @MessagePattern({ cmd: 'get:email' })
   @Get(':email')
+  @ApiOperation({ summary: 'Obtener usuario por correo electrónico' })
+  @ApiResponse({ status: 200, description: 'Usuario encontrado.', type: User })
+  @ApiResponse({ status: 404, description: 'Usuario no encontrado.' })
   async getUserByEmail(@Param('email') email: string): Promise<User> {
     return await this.usersService.getUserByEmail(email);
   }
